7e86c179562ab34b7c465a147ba729784f006f67
isaccbarker <milobanks@rowlandhall.org>
2023-12-06 16:25:16 -0700

    [common] format some code

--- common/common/src/commonMain/kotlin/org/ironlions/common/things/Thing.kt
+++ common/common/src/commonMain/kotlin/org/ironlions/common/things/Thing.kt
@@ -3,32 +3,34 @@ package org.ironlions.common.things
 import java.util.UUID
 
 /**
- * Class that represents a thing on the field
+ * Class that represents a thing on the field. You can think of this as an entity; it wasn't called
+ * an entity because Teo objected and apparently he has power even though I do all the programming
+ * for this. This code will be bundled with hundreds of other changes so he won't catch this comment
+ * during review, I can be almost confident of that. Can you hear me Teo? No? Ha!
  *
  * TODO: Probably a make a LocalSpaceGroup to make defining groups of bounding boxes easier.
  *
  * @param humanName The name of this thing that is human readable.
+ * @param things Things that belong to this time.
  * @param adHocProperties Other properties of this object.
  * @param uuid The UUID used by Panopticon and some internal parts of Sovereign. **Must be UNIQUE.**
  */
 abstract class Thing(
     val humanName: String,
+    val things: List<Thing> = listOf(),
     val adHocProperties: MutableList<Property.AdHoc> = mutableListOf(),
     val uuid: UUID = UUID.randomUUID(),
 ) {
     abstract class Property() {
         var note: String? = null
 
-        class Region(val region: org.ironlions.common.geometry.Region) :
-            Property()
+        class Region(val region: org.ironlions.common.geometry.Region) : Property()
 
-        class Color(val r: Float, val g: Float, val b: Float) :
-            Property()
+        class Color(val r: Float, val g: Float, val b: Float) : Property()
 
         class Model(val model: String) : Property()
 
-        class AdHoc(val data: Pair<String, String>) :
-            Property()
+        class AdHoc(val data: Pair<String, String>) : Property()
 
         fun note(note: String) = apply { this.note = note }
     }
@@ -40,6 +42,7 @@ abstract class Thing(
      * @param region The region that this object occupies.
      * @param color The color of this object.
      * @param model The model of this object (display only).
+     * @param things Things that belong to this thing.
      * @param adHocProperties Other properties of this object.
      */
     open class Concrete(
@@ -47,16 +50,20 @@ abstract class Thing(
         val region: Property.Region,
         val color: Property.Color = Property.Color(1f, 1f, 1f),
         val model: Property.Model = Property.Model("bounding"),
+        things: List<Thing> = listOf(),
         adHocProperties: MutableList<Property.AdHoc> = mutableListOf()
-    ) : Thing(humanName, adHocProperties)
+    ) : Thing(humanName, things, adHocProperties)
 
     /**
      * A field thing that does not exist in space.
      *
      * @param humanName The human-friendly name of this object.
+     * @param things Things that belong to this thing.
      * @param adHocProperties Other properties of this object.
      */
     open class Abstract(
-        humanName: String, adHocProperties: MutableList<Property.AdHoc> = mutableListOf()
-    ) : Thing(humanName, adHocProperties)
+        humanName: String,
+        things: List<Thing> = listOf(),
+        adHocProperties: MutableList<Property.AdHoc> = mutableListOf()
+    ) : Thing(humanName, things, adHocProperties)
 }
